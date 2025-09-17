9e6db5f8f6dced9652b122264908207e76f515ce
vyshnavi <vishuchowdary20@gmail.com>
2025-09-17 19:17:13 +0530

    registration pf user created

---
+++ Frontend/Frontend/src/Pages/Register.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import API from "../Services/api";
+import { useAuth } from "../Context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+export default function Register() {
+  const [form, setForm] = useState({ name: "", email: "", password: "" });
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await API.post("/auth/register", form);
+    login(data);
+    navigate("/items");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Register</h2>
+      <input
+        type="text"
+        placeholder="Name"
+        onChange={(e) => setForm({ ...form, name: e.target.value })}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setForm({ ...form, email: e.target.value })}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setForm({ ...form, password: e.target.value })}
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+}
