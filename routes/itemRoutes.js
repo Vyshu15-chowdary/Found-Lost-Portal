acf57425e19aad0aa562d47a1283d52ac88a6794
vyshnavi <vishuchowdary20@gmail.com>
2025-09-17 15:29:12 +0530

    routes for items created

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,1 +1,16 @@
+import express from "express";
+import {
+  createItem,
+  getItems,
+  getItemById,
+  deleteItem,
+} from "../controllers/itemController.js";
+import { protect } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.route("/").get(getItems).post(protect, createItem);
+router.route("/:id").get(getItemById).delete(protect, deleteItem);
+
+export default router;
 
