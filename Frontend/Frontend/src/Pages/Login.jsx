ec02684630ae53405c19affeaf8b7b48a8bb2c28
vyshnavi <vishuchowdary20@gmail.com>
2025-09-17 19:18:01 +0530

    user login created

---
+++ Frontend/Frontend/src/Pages/Login.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import API from "../Services/api";
+import { useAuth } from "../Context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [form, setForm] = useState({ email: "", password: "" });
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await API.post("/auth/login", form);
+    login(data);
+    navigate("/items");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setForm({ ...form, email: e.target.value })}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setForm({ ...form, password: e.target.value })}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
