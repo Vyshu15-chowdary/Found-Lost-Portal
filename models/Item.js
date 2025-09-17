30673cbf7ab3ac74dffed9d1b50e9e7e9eeabda1
vyshnavi <vishuchowdary20@gmail.com>
2025-09-17 15:13:16 +0530

    schema created

--- models/Item.js
+++ models/Item.js
@@ -1,1 +1,16 @@
+import mongoose from "mongoose";
+
+const itemSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    location: { type: String, required: true },
+    status: { type: String, enum: ["lost", "found"], required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+const Item = mongoose.model("Item", itemSchema);
+export default Item;
 
